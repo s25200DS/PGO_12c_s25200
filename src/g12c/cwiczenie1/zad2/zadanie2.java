748e74aaa902830e792a6990e75ad3133e283e5c
Dawid <s25200@pjwstk.edu.pl>
2022-03-13 16:01:53 +0100

    Update zadanie2.java

--- src/g12c/cwiczenie1/zad2/zadanie2.java
+++ src/g12c/cwiczenie1/zad2/zadanie2.java
@@ -8,7 +8,7 @@ public static void  main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
 
-    int a = scanner.nextInt();asdasd
+    int a = scanner.nextInt();
     int b = scanner.nextInt();
     int c = scanner.nextInt();
 
