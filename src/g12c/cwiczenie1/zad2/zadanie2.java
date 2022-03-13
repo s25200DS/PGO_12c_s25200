54b58e00b5172c15852763aafc726c3af9183db6
Dawid Szejbut <s25200@pjwstk.edu.pl>
2022-03-13 15:58:10 +0100

    test3

--- src/g12c/cwiczenie1/zad2/zadanie2.java
+++ src/g12c/cwiczenie1/zad2/zadanie2.java
@@ -1,2 +1,23 @@
-package g12c.cwiczenie1.zad2;public class zadanie2 {
+package g12c.cwiczenie1.zad2;
+
+import java.util.Scanner;
+
+public class zadanie2 {
+
+public static void  main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    int a = scanner.nextInt();asdasd
+    int b = scanner.nextInt();
+    int c = scanner.nextInt();
+
+    System.out.println(a + " " + b + " " + c);
+
+    if(a > b && a> c)
+    {
+
+    }
+}
+
 }
