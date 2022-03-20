4021ab70dea0a68cfbf284de4ec05c5e1c85e170
Dawid <s25200@pjwstk.edu.pl>
2022-03-20 18:44:34 +0100

    relokacja
    migracja danych

---
+++ src/g12c/cwiczenia/niedziela13mar2022/zadanie_2.java
@@ -0,0 +1,22 @@
+package g12c.cwiczenia.niedziela13mar2022;
+
+import java.util.Scanner;
+
+public class zadanie_2 {
+
+    public static void  main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        System.out.println(a + " " + b + " " + c);
+
+        if(a > b && a> c)
+        {
+
+        }
+    }
+}
